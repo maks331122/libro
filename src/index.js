7aae883ea266ff79994b182caa2f25a9c8e1d046
maks331122 <maks331122@ukr.net>
2023-10-15 21:41:25 +0300

    v0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import App from "./components/App";
 import './styles/index.css'
 
 const app = ReactDOM.createRoot(document.getElementById("app"));
-console.log(app);
-console.log(App);
 app.render(<App />);
 
